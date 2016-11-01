47ee318b0f6cac0f0482ed128c05a8b3c3fe1ef1
Dress <Dress@10.50.131.59>
2016-11-01 13:17:47 +0100

    create user

--- src/main/java/facades/UserFacade.java
+++ src/main/java/facades/UserFacade.java
@@ -12,35 +12,53 @@ import security.PasswordStorage;
 
 public class UserFacade implements IUserFacade {
 
-  EntityManagerFactory emf;
+    EntityManagerFactory emf;
 
-  public UserFacade(EntityManagerFactory emf) {
-    this.emf = emf;   
-  }
-
-  private EntityManager getEntityManager() {
-    return emf.createEntityManager();
-  }
-
-  @Override
-  public IUser getUserByUserId(String id) {
-    EntityManager em = getEntityManager();
-    try {
-      return em.find(User.class, id);
-    } finally {
-      em.close();
+    public UserFacade(EntityManagerFactory emf) {
+        this.emf = emf;
     }
-  }
 
-  @Override
-  public List<String> authenticateUser(String userName, String password) {
-    IUser user = getUserByUserId(userName);
-    try {
-      return user != null && PasswordStorage.verifyPassword(password, user.getPassword()) ? user.getRolesAsStrings() : null;
-    } catch (PasswordStorage.CannotPerformOperationException | PasswordStorage.InvalidHashException ex) {
-      Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
-      return null;
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
     }
-  }
+
+    @Override
+    public IUser getUserByUserId(String id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(User.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<String> authenticateUser(String userName, String password) {
+        IUser user = getUserByUserId(userName);
+        try {
+            return user != null && PasswordStorage.verifyPassword(password, user.getPassword()) ? user.getRolesAsStrings() : null;
+        } catch (PasswordStorage.CannotPerformOperationException | PasswordStorage.InvalidHashException ex) {
+            Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public IUser createUser(String userName, String password) {
+        try {
+            EntityManager em = getEntityManager();
+            em.getTransaction().begin();
+            IUser temp = getUserByUserId(userName);
+            User user = new User(userName, password);
+            if (temp == null) {
+                em.persist(user);
+                em.getTransaction().commit();
+                return user;
+            }
+            return null;
+        } catch (PasswordStorage.CannotPerformOperationException ex) {
+            Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
 
 }
