0d9af92efa98bd7ae7d7f805c3afc893a98ea3fe
Dress <Dress@10.50.131.59>
2016-11-02 12:13:18 +0100

    change of view / restricted

--- src/main/java/rest/Admin.java
+++ src/main/java/rest/Admin.java
@@ -8,7 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("demoadmin")
+@Path("admin")
 @RolesAllowed("Admin")
 public class Admin {
   
--- src/main/webapp/app/view5/view5.js
+++ src/main/webapp/app/view5/view5.js
@@ -2,8 +2,20 @@
 
 angular.module('myApp.view5', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view5', {
-    templateUrl: 'app/view5/view5.html'
-  });
-}]);
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view5', {
+                    templateUrl: 'app/view5/view5.html',
+                    controller: 'View5Ctrl'
+                });
+            }])
+
+        .controller('View5Ctrl', function ($http, $scope, $location) {
+            $http.get('api/admin')
+                    .success(function (data, status, headers, config) {
+                        $scope.data = data;
+                    })
+                    .error(function (data, status, headers, config) {
+                        $location.path('/view1')
+                    });
+
+        });
