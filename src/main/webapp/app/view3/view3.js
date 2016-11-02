3da752b418920ce8048ad20f7d4990f208ea5fb1
laurahartig <laurahartig@10.50.138.18>
2016-11-02 13:20:55 +0100

    again?

--- src/main/webapp/app/view3/view3.js
+++ src/main/webapp/app/view3/view3.js
@@ -2,20 +2,20 @@
 
 angular.module('myApp.view3', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view3', {
-    templateUrl: 'app/view3/view3.html',
-    controller: 'View3Ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view3', {
+                    templateUrl: 'app/view3/view3.html',
+                    controller: 'View3Ctrl'
+                });
+            }])
 
-.controller('View3Ctrl', function($http,$scope) {
-  $http.get('api/demoadmin')
-            .success(function (data, status, headers, config) {
-              $scope.data = data;
-            })
-            .error(function (data, status, headers, config) {
-              
-             });
- 
-});
+        .controller('View3Ctrl', function ($http, $scope, $location) {
+            $http.get('api/user')
+                    .success(function (data, status, headers, config) {
+                        $scope.data = data;
+                    })
+                    .error(function (data, status, headers, config) {
+                        $location.path('/view1')
+                    });
+
+        });
--- src/main/webapp/app/view4/view4.js
+++ src/main/webapp/app/view4/view4.js
@@ -2,8 +2,19 @@
 
 angular.module('myApp.view4', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view4', {
-    templateUrl: 'app/view4/view4.html'
-  });
-}]);
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view4', {
+                    templateUrl: 'app/view4/view4.html',
+                    controller: 'View4Ctrl'
+                });
+            }])
+
+        .controller('View4Ctrl', function ($http, $scope, $location) {
+            $http.get('api/user')
+                    .success(function (data, status, headers, config) {
+                        $scope.data = data;
+                    })
+                    .error(function (data, status, headers, config) {
+                        $location.path('/view1')
+                    });
+        });
